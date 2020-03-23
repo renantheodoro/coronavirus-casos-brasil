adc57c325aea758aacf805866e61c60666205d06
Renan Theodoro <renan.b.theodoro@gmail.com>
2020-03-22 22:34:28 -0300

    Release v1.0.2 (new API applied)

--- src/components/Loader.vue
+++ src/components/Loader.vue
@@ -1,53 +1,55 @@
 <template>
   <div class="spinner-box">
-    <!-- <div class="circle-border">
-      <div class="circle-core"></div>
-    </div>
-     -->
-
-    <!-- <div class="leo-border-1">
-      <div class="leo-core-1"></div>
-    </div>
-    <div class="leo-border-2">
-      <div class="leo-core-2"></div>
-    </div> -->
-
-    <!-- <div class="configure-border-1">
-      <div class="configure-core"></div>
-    </div>
-    <div class="configure-border-2">
-      <div class="configure-core"></div>
-    </div> -->
+    <p class="loader-message" v-html="messages[messageIndex]"></p>
 
     <div class="pulse-container">
       <div class="pulse-bubble pulse-bubble-1"></div>
       <div class="pulse-bubble pulse-bubble-2"></div>
       <div class="pulse-bubble pulse-bubble-3"></div>
     </div>
-
-    <!-- <div class="solar-system">
-      <div class="earth-orbit orbit">
-        <div class="planet earth"></div>
-        <div class="venus-orbit orbit">
-          <div class="planet venus"></div>
-          <div class="mercury-orbit orbit">
-            <div class="planet mercury"></div>
-            <div class="sun"></div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="blue-orbit leo"></div>
-
-    <div class="green-orbit leo"></div>
-
-    <div class="red-orbit leo"></div>
-
-    <div class="white-orbit w1 leo"></div>
-    <div class="white-orbit w2 leo"></div>
-    <div class="white-orbit w3 leo"></div>
-
-     -->
   </div>
 </template>
+
+<script>
+export default {
+  name: 'Loader',
+
+  data() {
+    return {
+      messageIndex: 0,
+      messages: [
+        `<strong>Aguarde só um momento</strong> <br> Estamos carregando os dados...`,
+        `<strong>O servidor pode estar lento</strong> <br> Espere só mais um momento...`,
+        `<strong>Se estiver demorando</strong> <br> Verifique sua conexão com à internet.`
+      ]
+    };
+  },
+
+  methods: {
+    updateMessage() {
+      if (this.messageIndex >= this.messages.length - 1) {
+        this.messageIndex = 0;
+      } else {
+        this.messageIndex++;
+      }
+    }
+  },
+
+  mounted() {
+    const self = this;
+    let initialTime = 1000;
+    const maxTime = 4000;
+
+    const loaderInterval = setInterval(() => {
+      initialTime += 1000;
+
+      if (initialTime >= maxTime) {
+        self.$emit('timedOut');
+        clearInterval(loaderInterval);
+      } else {
+        self.updateMessage();
+      }
+    }, 10000);
+  }
+};
+</script>
